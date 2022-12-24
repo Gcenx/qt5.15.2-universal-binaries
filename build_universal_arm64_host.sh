1cfd4babed22510c80a396cf0da78d08a601c984
Dean M Greer <38226388+Gcenx@users.noreply.github.com>
2022-12-24 01:00:04 -0500

    start refactoring

--- build_universal_arm64_host.sh
+++ build_universal_arm64_host.sh
@@ -1,5 +1,9 @@
 # TODO grab MacOSX11.0.sdk and put in /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs 
 # TODO change to fork of qt5 with patches on the 5.15.2 branch
+#!/usr/bin/env bash
+
+export JOBS="-j$(sysctl -n hw.activecpu)"
+
 git clone git://code.qt.io/qt/qt5.git
 cd qt5
 git checkout 5.15.2
@@ -7,32 +11,32 @@ git checkout 5.15.2
 cd ..
 mkdir qt5-build-x86_64
 cd qt5-build-x86_64
- ../qt5/configure -device-option QMAKE_APPLE_DEVICE_ARCHS=x86_64 -opensource -confirm-license -nomake examples -nomake tests -no-openssl -securetransport -prefix /usr/local/Qt-5.15.2-64 -skip qtwebengine
-make -j8
-sudo make install
+ ../qt5/configure -device-option QMAKE_APPLE_DEVICE_ARCHS=x86_64 -opensource -confirm-license -nomake examples -nomake tests -no-openssl -securetransport -skip qtwebengine
+make $JOBS
+make install
 cd qttools
-make -j8
-sudo make install
+make $JOBS
+make install
 cd ..
 cd qtmacextras
-make -j8
-sudo make install
+make $JOBS
+make install
 cd ..
 cd ..
 mkdir qt5-build-arm64
 cd qt5-build-arm64
 ../qt5/configure QMAKE_APPLE_DEVICE_ARCHS=arm64 -opensource -confirm-license -nomake examples -nomake tests -no-openssl -securetransport -skip qtwebengine -skip qt3d
-make -j8
-sudo make install 
+make $JOBS
+make install 
 cd qttools
-make -j8
-sudo make install
+make $JOBS
+make install
 cd ..
 cd qtmacextras
-make -j8
-sudo make install
+make $JOBS
+make install
 cd ..
 cd ..
 wget https://raw.githubusercontent.com/crystalidea/macdeployqt-universal/main/bin/makeuniversal
 chmod +x makeuniversal
-sudo ./makeuniversal "/usr/local/Qt-5.15.2-universal" "/usr/local/Qt-5.15.2-64" "/usr/local/Qt-5.15.2"
+./makeuniversal "/usr/local/Qt-5.15.2-universal" "/usr/local/Qt-5.15.2-64" "/usr/local/Qt-5.15.2"
